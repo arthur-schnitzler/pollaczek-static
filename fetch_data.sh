4567241bfa570f1fd6812c5afc3af82c68433015
csae8092 <peter.andorfer@oeaw.ac.at>
2022-11-30 11:52:52 +0100

    fetch data from pollaczek data repo

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,6 +1,6 @@
 rm -rf data
-wget https://github.com/arthur-schnitzler/pollaczek/archive/refs/heads/main.zip
+wget https://github.com/arthur-schnitzler/pollaczek-data/archive/refs/heads/main.zip
 unzip main.zip && rm main.zip
-mv pollaczek-main/data ./data
-rm -rf pollaczek-main
+mv pollaczek-data-main/data ./data
+rm -rf pollaczek-data-main
 ./dl_imprint.sh
