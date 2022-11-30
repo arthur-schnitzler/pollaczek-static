6ae5d6283e88e627447190f0f4d776610d551419
csae8092 <peter.andorfer@oeaw.ac.at>
2022-11-30 13:57:22 +0100

    wip: typesnse index is populated #1

--- process.sh
+++ process.sh
@@ -4,3 +4,6 @@ add-attributes -g "./data/indices/*.xml" -b "https://id.acdh.oeaw.ac.at.at/polla
 
 echo "make calendar data"
 python make_calendar_data.py
+
+echo "make typesense index"
+python make_typesense_index.py
