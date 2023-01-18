4da12de265ed688929d6091360da85132d81c2be
Martin Anton Müller <martin.anton.mueller@oeaw.ac.at>
2023-01-18 23:24:18 +0100

    bibliografie dazu

--- fetch_data.sh
+++ fetch_data.sh
@@ -3,6 +3,7 @@ curl -LO https://github.com/arthur-schnitzler/pollaczek-data/archive/refs/heads/
 unzip main.zip && rm main.zip
 mv pollaczek-data-main/data ./data
 mv pollaczek-data-main/bibliografie/listBibl.xml ./data
+mv pollaczek-data-main/bibliografie/bibliografie.xml ./data
 mv pollaczek-data-main/data/meta/CP-aufsatz.xml ./data/meta
 rm -rf pollaczek-data-main
 ./dl_imprint.sh
